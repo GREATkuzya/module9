8cba919eeca1412b190b0cf5514ad36d90609ce1
GREATkuzya <greatkuzya@gmail.com>
2021-11-24 23:45:15 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -23,75 +23,57 @@
 let divNode = document.querySelector('.div');
 let btnNode = document.querySelector('.button');
 
-btnNode.addEventListener('click', () => {
+let value;
 
-    let a = document.querySelector('.input1').value;
-    let b = document.querySelector('.input2').value;
-   
-    switch (true) {
-      case (a > 10 && b >=1 && b <=10) : 
-        let err2 = `<br><span>«Страница вне диапазона от 1 до 10»</span>`;
-        divNode.innerHTML = err2;
-        break;
-      
-      case (a >= 1 && a <= 10 && b >10) :
-        let err = `<br><span>«Лимит вне диапазона от 1 до 10»</span>`;
-        divNode.innerHTML = err;
-        break;
-      
-      case (a > 10 && b >10) : 
-        let err1 = `<br><span>«Страница и лимит вне диапазона от 1 до 10»</span>`;
-        divNode.innerHTML = err1;
-        break;
-        
-      default: 
-        let xhr = new XMLHttpRequest();
-          xhr.open('GET', `https://picsum.photos/v2/list?page=${a}&limit=${b}`, true);
+function checkLimit(value) {
+  if (value<=10 && value>=1){
+ return true;
+ } else {
+ return false;
+ }
+ }
+
+function sendXHR(pageField, limitField) {
+  let xhr = new XMLHttpRequest();
+          xhr.open('GET', `https://picsum.photos/v2/list?page=${pageField}&limit=${limitField}`, true);
           xhr.send();
           xhr.onload = function() {
+            if (xhr.status != 200) { 
+              console.log(`Error`);
+            } else {
           let result = JSON.parse(xhr.response);
-                console.log(result);
+          gallery(result);
+                                        }                                                       
+}}
+
+function gallery(result) {
                 let cards = '';
                   result.forEach(item => {
                       let cardBlock = `<div class="card"><img src="${item.download_url}" class="card-image"></div>`;
                   cards +=  cardBlock;
                 });         
                 divNode.innerHTML = cards;
-                        }}      
-     
-
-      
-      
-      
-     
+}
 
 
 
-// if (a > 10 && b >=1 && b <=10) {
-//   let err2 = `<br><span>«Страница вне диапазона от 1 до 10»</span>`;
-//   divNode.innerHTML = err2;
-// }
-// else if (a >= 1 && a <= 10 && b >=10) {
-//   let err = `<br><span>«Лимит вне диапазона от 1 до 10»</span>`;
-//   divNode.innerHTML = err;
-// }
-// else if (a > 10 && b >10) {
-//   let err1 = `<br><span>«Страница лимит вне диапазона от 1 до 10»</span>`;
-//   divNode.innerHTML = err1;}
-// else {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('GET', `https://picsum.photos/v2/list?page=${a}&limit=${b}`, true);
-//     xhr.send();
-//     xhr.onload = function() {
-//     let result = JSON.parse(xhr.response);
-//           console.log(result);
-//           let cards = '';
-//             result.forEach(item => {
-//                 let cardBlock = `<div class="card"><img src="${item.download_url}" class="card-image"></div>`;
-//             cards +=  cardBlock;
-//           });         
-//           divNode.innerHTML = cards;
-//                    }
-//   }
-})
+ btnNode.addEventListener('click', () => {
+  let pageField = document.querySelector('.input1').value;
+  let limitField = document.querySelector('.input2').value;
+  let limitPage=checkLimit(pageField);
+  console.log(limitPage);
+  let limitLimit=checkLimit(limitField);
+  console.log(limitLimit);
+  if (limitPage && limitLimit) {
+    sendXHR(pageField, limitField);
+ } else if (!limitPage && !limitLimit) {
+    divNode.innerHTML=`<br><span>«Страница и лимит вне диапазона от 1 до 10»</span>`; }
+ else if (limitPage) {
+   divNode.innerHTML=`<br><span>«Лимит вне диапазона от 1 до 10»</span>`;
+ } else {
+   divNode.innerHTML= `<br><span>«Страница вне диапазона от 1 до 10»</span>`;
+ }
+ }); 
 
+
+   
